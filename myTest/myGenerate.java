2c9f8e01dc62f7a0c6fd1ca67e3491365aee7874
Xing <x.fan.cn@gmail.com>
2015-01-08 15:28:15 +1300

    add basic exception handling test case

--- myTest/myGenerate.java
+++ myTest/myGenerate.java
@@ -1,13 +1,13 @@
 import java.io.*;
 
-
 public class myGenerate extends Object{
 	
 	public static void main (String[] args) {
 		// TODO Auto-generated method stub
 		try {
 			//pj.PyjamaToJavaParser.parse(new File("myTest/myTest.pj"));
-			pj.PyjamaToJavaParser.parse(new File("myTest/squares/Squares.pj"));
+			//pj.PyjamaToJavaParser.parse(new File("myTest/squares/Squares.pj"));
+			pj.PyjamaToJavaParser.parse(new File("myTest/exceptionTest/ExceptionTest.pj"));
 			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
