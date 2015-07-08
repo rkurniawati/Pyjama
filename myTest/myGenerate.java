f49992ba32bc0ea0c739d4aaf37353e4b6bcad35
Xing <x.fan.cn@gmail.com>
2015-07-09 01:08:06 +1200

    update EPCCsyncbench class

--- myTest/myGenerate.java
+++ myTest/myGenerate.java
@@ -5,8 +5,7 @@ public class myGenerate extends Object{
 	public static void main (String[] args) {
 		// TODO Auto-generated method stub
 		try {
-			pj.PyjamaToJavaParser.parse(new File("myTest/HelloWorld.pj));
-
+			pj.PyjamaToJavaParser.parse(new File("myTest/exceptionTest/EPCCsyncbench.pj"));
 			//pj.PyjamaToJavaParser.parse(new File("myTest/exceptionTest/CompareBench.pj"));
 			//pj.PyjamaToJavaParser.parse(new File("myTest/exceptionTest/HexaBench.pj"));
 			//pj.PyjamaToJavaParser.parse(new File("myTest/exceptionTest/SyncBenchPar.pj"));
